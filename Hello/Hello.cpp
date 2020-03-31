d268f054b582667065fedb370ab6de535b0b8afb
Williano <paawilly17@gmail.com>
2020-03-31 16:11:49 -0500

    Add program description

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -1,10 +1,18 @@
+/*
+    Program: Hello
+    Description: A C++ program that asks the user to enter his or her name.
+                 The program then respond with a message that says hello to
+                 the user, using his or her name.
+    Programmer: William Kpabitey Kwabla
+    Date: 03/30/20
+*/
+
 #include <iostream>
 #include <string>
 
 // Function prototypes to enable main function to call functions before declaration.
 std::string getUserName();
 void displayHelloMessageWithUsername(std::string userName);
-
 
 int main()
 {
