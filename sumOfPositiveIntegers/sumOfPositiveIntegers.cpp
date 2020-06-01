2d5d61d52831c893ebe48e227bd691ddfd4fb359
Williano <paawilly17@gmail.com>
2020-06-01 06:59:57 -0500

    Add function implementation

--- sumOfPositiveIntegers/sumOfPositiveIntegers.cpp
+++ sumOfPositiveIntegers/sumOfPositiveIntegers.cpp
@@ -1,1 +1,44 @@
+/**
+    Program: Sum of the First n Positive Integers
+    Description: A C++ program that  reads a positive integer, n, from the user
+                 and then displays the sum of all of the integers from 1 to n.
+    Programmer: William Kpabitey Kwabla
+    Date: 06/01/20
+*/
 
+#include <iostream>
+#include <string>
+
+// Function prototypes
+int getPositiveIntegerFromUser();
+int computeSumOfPositiveIntegers();
+void displaySumOfPositiveIntegers();
+
+
+void main() {
+
+}
+
+
+int getPositiveIntegerFromUser()
+{
+    int userPositiveInteger;
+
+    std::cout << "Please enter the positive integer you want to compute the sum for: " ;
+
+    std::cin >> userPositiveInteger;
+
+    return userPositiveInteger;
+}
+
+
+int computeSumOfPositiveIntegers()
+{
+
+}
+
+
+void displaySumOfPositiveIntegers()
+{
+
+}
