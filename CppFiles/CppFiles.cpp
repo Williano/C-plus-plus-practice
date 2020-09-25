546d5ed8fa67b8924b653b522773c58ebe49f457
Williano <paawilly17@gmail.com>
2020-09-24 19:55:41 -0500

    Add writing to file example

---
+++ CppFiles/CppFiles.cpp
@@ -0,0 +1,61 @@
+// CppFiles.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+// Function prototypes
+void writeToFile();
+void writeUserInputToFile();
+
+int main()
+{
+
+	writeUserInputToFile();
+
+}
+
+void writeToFile()
+{
+	std::ofstream outputFile;
+	outputFile.open("demofile.txt");
+
+	std::cout << "Now writing data to the file. \n";
+
+	outputFile << "Beach\n";
+	outputFile << "Bee\n";
+	outputFile << "Mozart\n";
+	outputFile << "Schubert\n";
+
+	outputFile.close();
+
+	std::cout << "Done\n";
+}
+
+void writeUserInputToFile()
+{
+	std::ofstream outputFile;
+
+	int number1, number2, number3;
+
+	outputFile.open("Numbers.txt");
+
+	std::cout << "Enter a number: ";
+	std::cin >> number1;
+
+	std::cout << "Enter another number: ";
+	std::cin >> number2;
+
+	std::cout << "Enter another number: ";
+	std::cin >> number3;
+
+	outputFile << number1 << std::endl;
+	outputFile << number2 << std::endl;
+	outputFile << number3 << std::endl;
+
+	std::cout << "The numbers were saved to a file.\n";
+
+	outputFile.close();
+	std::cout << "Done.\n";
+
+}
