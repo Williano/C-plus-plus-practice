dd0e6a6642ca9cf75e224e056bbdd14efe68bc7b
Williano <paawilly17@gmail.com>
2021-02-03 16:52:28 -0600

    Add example implementation for class

---
+++ Classes/Src/RectangleExample.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+#include "Rectangle.h"
+
+int main()
+{
+	double houseWidth, houseLength;
+
+	std::cout << "In feet, how wide is your house? ";
+	std::cin >> houseWidth;
+
+	std::cout << "In feet, how long is your house? ";
+	std::cin >> houseLength;
+
+	std::cout << " " << std::endl;
+
+	Rectangle house(houseWidth, houseLength);
+
+	std::cout << "This is the details about your house: " << std::endl;
+	std::cout << " " << std::endl;
+
+	std::cout << std::setprecision(2) << std::fixed;
+	std::cout << "The house is " << house.getWidth() << " feet wide." << std::endl;
+
+	std::cout << "The house is " << house.getLength() << " feet long." << std::endl;
+
+	std::cout << "The house is " << house.getArea() << " square feet in area." << std::endl;
+
+
+	// Wait for user to press any key before exiting.
+	std::cin.get();
+
+	return 0;
+
+}
