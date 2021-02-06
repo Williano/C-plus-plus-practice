2ee5c971358e22753db4bac27e2c82d4278569f6
Williano <paawilly17@gmail.com>
2021-02-05 20:41:06 -0600

    Add class declaration

--- ObjectOrientedProgramming/FriendsOfClasses/Src/Budget.h
+++ ObjectOrientedProgramming/FriendsOfClasses/Src/Budget.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "Auxil.h"
+
+class Budget
+{
+private:
+	static double m_corpBudget;
+	double m_divisionBudget;
+
+public:
+	Budget()
+	{
+		m_divisionBudget = 0;
+	}
+
+	void addBudget(double budget)
+	{
+		m_divisionBudget += budget;
+		m_corpBudget += budget;
+	}
+
+	double getDivisionBudget() const
+	{
+		return m_divisionBudget;
+	}
+
+	double getCorpBudget() const
+	{
+		return m_corpBudget;
+	}
+
+	static void mainOffice(double mainOfficeRequest);
+
+	friend void AuxiliaryOffice::addBudget(double budget, Budget& corpBudget);
+};
