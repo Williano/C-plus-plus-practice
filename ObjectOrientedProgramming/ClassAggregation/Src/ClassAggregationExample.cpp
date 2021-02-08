21457bdedf593bbfbe37ebf9fd0fb77fd6a5f07f
Williano <paawilly17@gmail.com>
2021-02-08 17:14:15 -0600

    Add example of class aggregation

---
+++ ObjectOrientedProgramming/ClassAggregation/Src/ClassAggregationExample.cpp
@@ -0,0 +1,12 @@
+﻿#include "Course.h"
+
+int main()
+{
+	Course myCourse("Intro to Computer Science", "Kramer", "Shawn", "RH3010", "Starting out with C++", "Gaddis", "Addision-Wesley");
+
+	myCourse.print();
+
+	std::cin.get();
+
+	return 0;
+}
