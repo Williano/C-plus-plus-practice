5f24b3538ccbd9ac971017347da3edf0fbd901a3
Williano <paawilly17@gmail.com>
2021-02-08 16:37:33 -0600

    Refactor

--- ObjectOrientedProgramming/ClassAggregation/Src/Instructor.h
+++ ObjectOrientedProgramming/ClassAggregation/Src/Instructor.h
@@ -7,22 +7,22 @@
 class Instructor
 {
 private:
-	std::string lastName;
-	std::string firstName;
-	std::string officeNumber;
+	std::string m_lastName;
+	std::string m_firstName;
+	std::string m_officeNumber;
 
 public:
-	Instructor() : lastName(""), firstName(""), officeNumber("")
+	Instructor() : m_lastName(""), m_firstName(""), m_officeNumber("")
 	{
 
 	}
 
-	Instructor(std::string lName, std::string fName, std::string office) : lastName(lName), firstName(fName), officeNumber(office) {}
+	Instructor(std::string lName, std::string fName, std::string office) : m_lastName(lName), m_firstName(fName), m_officeNumber(office) {}
 
 	void print() const
 	{
-		std::cout << "Last name: " << lastName << std::endl;
-		std::cout << "First name: " << firstName << std::endl;
-		std::cout << "Office number " << officeNumber << std::endl;
+		std::cout << "Last name: " << m_lastName << std::endl;
+		std::cout << "First name: " << m_firstName << std::endl;
+		std::cout << "Office number " << m_officeNumber << std::endl;
 	}
 };
