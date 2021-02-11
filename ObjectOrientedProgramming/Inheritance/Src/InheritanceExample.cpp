f15522078e8779f318d849f7f6ec03fc9d5bfbd6
Williano <paawilly17@gmail.com>
2021-02-11 17:33:10 -0600

    Update

---
+++ ObjectOrientedProgramming/Inheritance/Src/InheritanceExample.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+
+#include "FinalExam.h"
+
+
+int main()
+{
+
+	int questions;
+	int missed;
+
+	std::cout << "How many questions are on the final exam? ";
+	std::cin >> questions;
+
+	std::cout << "How many questions did the student miss? ";
+	std::cin >> missed;
+
+	FinalExam test(questions, missed);
+
+	std::cout << std::setprecision(2);
+	std::cout << "\nEach question counts " << test.getPointsEach() << " points.\n";
+	std::cout << "The exam score is " << test.getScore() << std::endl;
+	std::cout << "The exam grade is " << test.getLetterGrade() << std::endl;
+
+	std::cin.get();
+
+	return 0;
+
+}
