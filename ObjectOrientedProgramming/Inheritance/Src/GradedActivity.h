df99c3632f33eb3b906acc09e82ea810199633c1
Williano <paawilly17@gmail.com>
2021-02-11 17:02:36 -0600

    Add GradedActivity class

---
+++ ObjectOrientedProgramming/Inheritance/Src/GradedActivity.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+
+
+class GradedActivity
+{
+private:
+	double m_score;
+
+public:
+	GradedActivity()
+	{
+		m_score = 0.0;
+	}
+
+	GradedActivity(double score) : m_score(score) {}
+
+	void setScore(double score)
+	{
+		m_score = score;
+	}
+
+	double getScore() const
+	{
+		return m_score;
+	}
+
+	char getLetterGrade() const;
+};
