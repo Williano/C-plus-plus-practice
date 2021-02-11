05a74ebacecb287b1f0b9e5b58a00a720c781546
Williano <paawilly17@gmail.com>
2021-02-11 17:19:42 -0600

    Add Final Exam implementation

---
+++ ObjectOrientedProgramming/Inheritance/Src/FinalExam.cpp
@@ -0,0 +1,15 @@
+﻿#include "FinalExam.h"
+
+void FinalExam::set(int questions, int missed)
+{
+	double numericScore;
+
+	m_numQuestions = questions;
+	m_numMissed = missed;
+
+	m_pointsEach = 100.0;
+
+	numericScore = 100 - (missed * m_pointsEach);
+
+	setScore(numericScore);
+}
