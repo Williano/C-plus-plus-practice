dd784277d1094a28949f98cc37adad5b5746ec2f
Williano <paawilly17@gmail.com>
2021-02-11 17:07:52 -0600

    Add GradedActivity implementation

---
+++ ObjectOrientedProgramming/Inheritance/Src/GradedActivity.cpp
@@ -0,0 +1,31 @@
+﻿#include "GradedActivity.h"
+
+char GradedActivity::getLetterGrade() const
+{
+	char letterGrade;
+
+	if (m_score > 89)
+	{
+		letterGrade = 'A';
+	}
+	else if (m_score > 79)
+	{
+		letterGrade = 'B';
+	}
+	else if (m_score > 69)
+	{
+		letterGrade = 'C';
+	}
+	else if (m_score > 59)
+	{
+		letterGrade = 'D';
+	}
+	else
+	{
+		letterGrade = 'F';
+	}
+
+
+	return letterGrade;
+
+}
