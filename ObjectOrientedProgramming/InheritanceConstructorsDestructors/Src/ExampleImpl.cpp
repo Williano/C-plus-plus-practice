fce64ca377b9a046687d89c3293d14bbb6c2f68c
Williano <paawilly17@gmail.com>
2021-02-12 15:37:19 -0600

    Add example

---
+++ ObjectOrientedProgramming/InheritanceConstructorsDestructors/Src/ExampleImpl.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "DerivedClass.h"
+
+
+int main()
+{
+	std::cout << "We will now define a DerivedClass object.\n";
+
+	DerivedClass object;
+
+	std::cout << "The program is now going to end.\n";
+
+	return 0;
+
+}
