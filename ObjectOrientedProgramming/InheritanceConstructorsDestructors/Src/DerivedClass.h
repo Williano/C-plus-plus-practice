9069a56aad046fc7ffc17644b2f45ebf92f1cf37
Williano <paawilly17@gmail.com>
2021-02-12 15:30:32 -0600

    Add derived class'

---
+++ ObjectOrientedProgramming/InheritanceConstructorsDestructors/Src/DerivedClass.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <iostream>
+#include "BaseClass.h"
+
+
+class DerivedClass : public BaseClass
+{
+public:
+
+	DerivedClass()
+	{
+		std::cout << "This is the DerivedClass constructor.\n";
+	}
+
+	~DerivedClass()
+	{
+		std::cout << "This is the DerivedClass destructor.\n";
+	}
+};
