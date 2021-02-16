2b894272050f43b53820113d371f35ae7e9df052
Williano <paawilly17@gmail.com>
2021-02-16 16:46:28 -0600

    Add derived class

---
+++ ObjectOrientedProgramming/OverrideFinalKeywords/Src/Derived.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <iostream>
+#include "Base.h"
+
+
+class Derived :public Base
+{
+public:
+
+	virtual void functionA(long arg) const override
+	{
+		std::cout << "This is Derived::funtionA" << std::endl;
+	}
+};
