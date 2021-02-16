b83751c15e70488868cc7bd52b992a82e5ca3a70
Williano <paawilly17@gmail.com>
2021-02-16 16:41:33 -0600

    Add Override and Final practice

---
+++ ObjectOrientedProgramming/OverrideFinalKeywords/Src/Base.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <iostream>
+
+
+class Base
+{
+public:
+
+	virtual void functionA(long arg) const
+	{
+		std::cout << "This is Derived::functionA" << std::endl;
+	}
+
+
+};
