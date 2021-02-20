f137b09adeef4fd13e79e00ad76eca5bf5b30649
Williano <paawilly17@gmail.com>
2021-02-20 17:46:18 -0600

    Add abstract class example

---
+++ ObjectOrientedProgramming/AbstractClasses/Src/AbstractClassExample.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "CsStudent.h"
+
+
+int main()
+{
+	CsStudent student("Jennifer Haynes", "167W98337", 2006);
+
+	student.setMathHours(12);
+	student.setCsHours(20);
+	student.setGenEdHours(40);
+
+	std::cout << "The student " << student.getName()
+		<< " needs to take " << student.getRemainingHours()
+		<< " more hours to graduate.\n";
+
+	return 0;
+}
