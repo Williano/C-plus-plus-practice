dda761d060a48be6c23db88064237cb3c76cb59e
Williano <paawilly17@gmail.com>
2021-02-20 17:39:51 -0600

    Add csStudent class implementation

---
+++ ObjectOrientedProgramming/AbstractClasses/Src/CsStudent.cpp
@@ -0,0 +1,10 @@
+﻿#include "CsStudent.h"
+
+int CsStudent::getRemainingHours() const
+{
+	int requiredHours, remainingHours;
+
+	requiredHours = MATH_HOURS + CS_HOURS + GEN_ED_HOURS;
+
+	remainingHours = requiredHours - (m_mathHours + m_csHours + m_genEdHours);
+}
