121ef3bcdf709e7ef40eaae9552918cd7096fbfb
Williano <paawilly17@gmail.com>
2021-03-08 16:46:14 -0600

    Add multiple inheritance example

---
+++ ObjectOrientedProgramming/MultipleInheritance/Src/MultipleInheritanceExample.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "DateTime.h"
+
+
+int main()
+{
+
+
+	DateTime emptyDay;
+
+	emptyDay.showDateTime();
+
+	DateTime pastDay(2, 4, 1960, 5, 32, 27);
+
+	pastDay.showDateTime();
+
+
+	std::cin.get();
+
+	return 0;
+}
