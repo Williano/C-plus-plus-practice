5512657ddb4f32583a5a8f2130eab3109ecdaa6b
Williano <paawilly17@gmail.com>
2021-03-08 16:40:07 -0600

    Add DateTime implementation

---
+++ ObjectOrientedProgramming/MultipleInheritance/Src/DateTime.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include "DateTime.h"
+
+
+DateTime::DateTime() : Date(), Time() {}
+
+DateTime::DateTime(int day, int month, int year, int hour, int min, int sec) : Date(day, month, year), Time(hour, min, sec) {}
+
+void DateTime::showDateTime() const
+{
+	std::cout << getMonth() << "/" << getDay() << "/" << getYear() << " ";
+}
