b7344ef3b1c92762f84757f9c15bee95027a48fb
Williano <paawilly17@gmail.com>
2021-03-08 16:30:52 -0600

    Add DateTime class

---
+++ ObjectOrientedProgramming/MultipleInheritance/Src/DateTime.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+#include "Date.h"
+#include "Time.h"
+
+
+class DateTime : public Date, public Time
+{
+
+public:
+	DateTime();
+
+	DateTime(int day, int month, int year, int hour, int min, int sec);
+
+	void showDateTime() const;
+};
