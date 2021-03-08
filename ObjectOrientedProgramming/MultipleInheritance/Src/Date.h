cdd298762bb6e110a0a45a87a9ab90de6b962dcd
Williano <paawilly17@gmail.com>
2021-03-08 16:20:08 -0600

    Add date class

---
+++ ObjectOrientedProgramming/MultipleInheritance/Src/Date.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+
+class Date
+{
+
+protected:
+	int m_day;
+	int m_month;
+	int m_year;
+
+public:
+
+	Date(int day, int month, int year) : m_day(1), m_month(1), m_year(1990) {}
+
+	Date(int day, int month, int year)
+	{
+		m_day = day;
+		m_month = month;
+		m_year = year;
+	}
+
+	int getDay() const
+	{
+		return m_day;
+	}
+
+	int getMonth() const
+	{
+		return m_month;
+	}
+
+	int getYear() const
+	{
+		return m_year;
+	}
+};
