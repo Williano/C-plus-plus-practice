229a96c2d760c82a557a24ba3747b7e39f37616f
Williano <paawilly17@gmail.com>
2021-04-22 17:04:01 -0500

    Add implementation

---
+++ ErrorHandling/Src/ErrorHandling.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+
+double divide(int num, int num2);
+
+
+int main()
+{
+	int num1, num2;
+	double quotient;
+
+	std::cout << "Enter two numbers: ";
+	std::cin >> num1 >> num2;
+
+	try
+	{
+		quotient = divide(num1, num2);
+		std::cout << "The quotient is " << quotient << std::endl;
+	}
+	catch (std::string exceptionString)
+	{
+		std::cout << exceptionString;
+		std::cout << "End of the program.\n";
+		return 0;
+	}
+
+}
+
+
+double divide(int numerator, int denominator)
+{
+	if (denominator == 0)
+	{
+		std::string exceptionString = "ERROR:: Cannot divide by zero.\n";
+		throw exceptionString;
+	}
+
+	return static_cast<double>(numerator) / denominator;
+}
